25c8c10a1faa812056a2a494a24ca3f3258162cf
ZhuangCd <126353820+ZhuangCd@users.noreply.github.com>
2024-02-23 10:18:36 +0000

    Chat history and streaming answer added
    (Seeing the autoregressive model word by word displayed on the screen)

--- chat.py
+++ chat.py
@@ -3,28 +3,37 @@ from typing import List
 from ctransformers import AutoModelForCausalLM
 
 
-def get_prompt(instruction: str, history: List[str] = None) -> str:
+def get_prompt(instruction: str, history: List[str]) -> str:
     system = "You are an Ai assistant that gives helpful anwers. You answer in short and concise way"
     prompt = f"### System:\n{system}\n\n### User:\n"
-    if history is not None:
+    if len(history) > 0:
         prompt += f"This is the conversation history: {''.join(history)}. Now answer the question: "
     prompt += f"{instruction}\n\n### Response:\n"
-    print(prompt)
     return prompt
+
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    message_history = cl.user_session.get("message_history")
+    msg = cl.Message(content="")
+    await msg.send()
+
+    prompt = get_prompt(message.content, message_history)
+    response = ""
+    for word in llm(prompt, stream=True):
+        await msg.stream_token(word)
+        response += word
+    await msg.update()
+    message_history.append(response)
 
 
 @cl.on_chat_start
 def on_chat_start():
+    cl.user_session.set("message_history", [])
     global llm
     llm = AutoModelForCausalLM.from_pretrained(
         "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
     )
-
-@cl.on_message
-async def on_message(message: cl.Message):
-    prompt = get_prompt(message.content)
-    response = llm(prompt)
-    await cl.Message(response).send()
 
 
 """
