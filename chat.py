223a69ee320ddcde2590a1eda5c27cb81079821b
ZhuangCd <126353820+ZhuangCd@users.noreply.github.com>
2024-02-23 09:21:06 +0000

    Added "global llm" under a function, but it stays global

--- chat.py
+++ chat.py
@@ -3,11 +3,6 @@ from typing import List
 from ctransformers import AutoModelForCausalLM
 
 
-llm = AutoModelForCausalLM.from_pretrained(
-    "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
-)
-
-
 def get_prompt(instruction: str, history: List[str] = None) -> str:
     system = "You are an Ai assistant that gives helpful anwers. You answer in short and concise way"
     prompt = f"### System:\n{system}\n\n### User:\n"
@@ -18,9 +13,17 @@ def get_prompt(instruction: str, history: List[str] = None) -> str:
     return prompt
 
 
+@cl.on_chat_start
+def on_chat_start():
+    global llm
+    llm = AutoModelForCausalLM.from_pretrained(
+        "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
+    )
+
 @cl.on_message
 async def on_message(message: cl.Message):
-    response = f"Hello, you just sent: {message.content}!"
+    prompt = get_prompt(message.content)
+    response = llm(prompt)
     await cl.Message(response).send()
 
 
